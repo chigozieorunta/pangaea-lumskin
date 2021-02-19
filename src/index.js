b954c5739ed30e244b6bd7c10efa8afead6fd130
Chigozie Orunta <chigozieorunta@yahoo.com>
2021-02-19 02:36:53 +0100

    Added new index and header styles

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Index from "../src/pages/index";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./fonts/freight-display/font.css";
-import "./fonts/ff-bau/font.css";
+import "./styles/index.css";
+import "./styles/header.css";
 
 ReactDOM.render(
   <React.StrictMode>
