e532f6cf16e40bc2fa5d1899ac5a1fe033afe709
Chigozie Orunta <chigozieorunta@yahoo.com>
2021-02-19 05:35:40 +0100

    Instantiated Apollo Client

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,23 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import HomePage from "./home";
+import { ApolloProvider } from "@apollo/client";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://pangaea-interviews.now.sh/api/graphql",
+  cache: new InMemoryCache(),
+});
 
 const Index = () => {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" component={HomePage} exact={true} />
-      </Switch>
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={HomePage} exact={true} />
+        </Switch>
+      </BrowserRouter>
+    </ApolloProvider>
   );
 };
 
