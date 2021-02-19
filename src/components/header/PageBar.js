24b3e0eaec8a1339fb1194d2a8fcfcc8cf3c02c4
Chigozie Orunta <chigozieorunta@yahoo.com>
2021-02-19 05:36:18 +0100

    Adjusted width of Page Bar

--- src/components/header/PageBar.js
+++ src/components/header/PageBar.js
@@ -22,24 +22,28 @@ const pageExcerptStyle = { fontSize: "1rem" };
 
 const PageBar = () => {
   return (
-    <Container style={headerStyle}>
+    <Container style={headerStyle} fluid>
       <Row>
-        <Col style={colStyle}>
-          <div>
-            <h1 style={pageTitleStyle}>All Products</h1>
-            <span style={pageExcerptStyle}>A 360° look at Lumin</span>
-          </div>
-          <select className="headerfilter">
-            <option value="" disabled="">
-              Filter By
-            </option>
-            <option value="all-products">All Products</option>
-            <option value="new-products">New Products</option>
-            <option value="sets">Sets</option>
-            <option value="skincare">Skincare</option>
-            <option value="hair-and-body">Hair &amp; Body Care</option>
-            <option value="accesories">Accesories</option>
-          </select>
+        <Col sm={{ span: 8, offset: 2 }}>
+          <Row>
+            <Col style={colStyle}>
+              <div>
+                <h1 style={pageTitleStyle}>All Products</h1>
+                <span style={pageExcerptStyle}>A 360° look at Lumin</span>
+              </div>
+              <select className="headerfilter">
+                <option value="" disabled="">
+                  Filter By
+                </option>
+                <option value="all-products">All Products</option>
+                <option value="new-products">New Products</option>
+                <option value="sets">Sets</option>
+                <option value="skincare">Skincare</option>
+                <option value="hair-and-body">Hair &amp; Body Care</option>
+                <option value="accesories">Accesories</option>
+              </select>
+            </Col>
+          </Row>
         </Col>
       </Row>
     </Container>
