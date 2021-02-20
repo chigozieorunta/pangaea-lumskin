97175536bf5e44ef00d99ee89e4a7046e4b41c8a
Chigozie Orunta <chigozieorunta@yahoo.com>
2021-02-20 15:02:21 +0100

    Added TopNavigationBar and PageBar components

--- src/layout/header.js
+++ src/layout/header.js
@@ -1,12 +1,12 @@
 import React from "react";
-import NavigationBar from "../components/header/NavigationBar";
+import TopNavigationBar from "../components/header/TopNavigationBar";
 import PageBar from "../components/header/PageBar";
 import Section from "./Section";
 
 const Header = () => {
   return (
     <Section bgcolor="#f5f5f4">
-      <NavigationBar />
+      <TopNavigationBar />
       <PageBar />
     </Section>
   );
