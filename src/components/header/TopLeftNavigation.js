595d7c2eb2fd95ef40335544db0be6bf2f61f134
Chigozie Orunta <chigozieorunta@yahoo.com>
2021-02-20 15:08:47 +0100

    Created TopLeftNavigation component

---
+++ src/components/header/TopLeftNavigation.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const menuStyle = {
+  listStyle: "none",
+  margin: 0,
+  padding: 0,
+  display: "flex",
+  justifyContent: "space-around",
+  alignItems: "center",
+  width: "15%",
+};
+
+const TopLeftNavigation = () => {
+  return (
+    <ul style={menuStyle}>
+      <li>
+        <Link to="/shop">Shop</Link>
+      </li>
+      <li>
+        <Link to="/help">Help</Link>
+      </li>
+      <li>
+        <Link to="/blog">Blog</Link>
+      </li>
+    </ul>
+  );
+};
+
+export default TopLeftNavigation;
