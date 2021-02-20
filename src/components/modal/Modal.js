3be76d26098d1d7e626f22ff9536be5e41b3a941
Chigozie Orunta <chigozieorunta@yahoo.com>
2021-02-20 17:02:05 +0100

    Cleaned and refactored Modal component

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -10,49 +10,6 @@ const Modal = ({ showModal, setShowModal, onChangeCart, cart }) => {
     setShowModal(!showModal);
   };
 
-  const modalStyle = {
-    top: 0,
-    left: showModal ? 0 : "-100%",
-    bottom: 0,
-    right: 0,
-    opacity: "0.8",
-    width: "100%",
-    height: "100%",
-    backgroundColor: "rgb(205, 209, 206)",
-    backdropFilter: "blur(3px)",
-    position: "fixed",
-    display: "flex",
-    justifyContent: "flex-end",
-  };
-
-  const modalContentAreaStyle = {
-    backgroundColor: "#f2f2ef",
-    width: 480,
-    maxWidth: "100%",
-    height: "100%",
-    opacity: "1",
-    backdropFilter: "none",
-    padding: "1.5em",
-    position: "fixed",
-    right: 0,
-    top: 0,
-    bottom: 0,
-    zIndex: 5000,
-  };
-
-  const modalTitle = {
-    position: "relative",
-    textAlign: "center",
-    textTransform: "uppercase",
-    color: "#696969",
-    letterSpacing: 1,
-    fontSize: 10,
-    textAlign: "center",
-    marginBottom: 0,
-    lineHeight: 1.2,
-    marginBottom: "3em",
-  };
-
   const animateModal = (status) => {
     return status
       ? { opacity: 1, transition: "all 0.4s", left: "100%" }
@@ -61,15 +18,25 @@ const Modal = ({ showModal, setShowModal, onChangeCart, cart }) => {
 
   useEffect(() => {
     setShoppingCart(cart);
-    console.log("The new shopping Cart");
-    console.log(shoppingCart);
   }, [cart]);
 
   return (
     <section style={animateModal(showModal)}>
-      <section style={modalStyle}></section>
-      <section style={modalContentAreaStyle}>
-        <div style={modalTitle}>
+      <section
+        style={{
+          top: 0,
+          left: showModal ? 0 : "-100%",
+          bottom: 0,
+          right: 0,
+          opacity: "0.8",
+          width: "100%",
+          backgroundColor: "rgb(205, 209, 206)",
+          backdropFilter: "blur(3px)",
+          position: "fixed",
+        }}
+      ></section>
+      <section style={styles.modalContentAreaStyle}>
+        <div style={styles.modalTitle}>
           <CloseIcon onClick={closeModal} />
           <span>Your Cart</span>
         </div>
@@ -93,6 +60,36 @@ const Modal = ({ showModal, setShowModal, onChangeCart, cart }) => {
       </section>
     </section>
   );
+};
+
+const styles = {
+  modalContentAreaStyle: {
+    backgroundColor: "#f2f2ef",
+    width: 480,
+    maxWidth: "100%",
+    height: "100%",
+    opacity: "1",
+    backdropFilter: "none",
+    padding: "1.5em",
+    position: "fixed",
+    right: 0,
+    top: 0,
+    bottom: 0,
+    zIndex: 5000,
+  },
+
+  modalTitle: {
+    position: "relative",
+    textAlign: "center",
+    textTransform: "uppercase",
+    color: "#696969",
+    letterSpacing: 1,
+    fontSize: 10,
+    textAlign: "center",
+    marginBottom: 0,
+    lineHeight: 1.2,
+    marginBottom: "3em",
+  },
 };
 
 export default Modal;
