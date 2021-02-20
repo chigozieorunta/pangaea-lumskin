e6849199aa44fe52e79fd1deb3d875e7761100a2
Chigozie Orunta <chigozieorunta@yahoo.com>
2021-02-20 15:01:33 +0100

    Added addToCart and onChangeCartFromModel methods to component

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../layout/Layout";
 import Content from "../components/content/Content";
+import Modal from "../components/modal/Modal";
 
 const HomePage = () => {
+  let [cart, setCart] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+
+  const loadModal = () => {
+    setShowModal(!showModal);
+  };
+
+  const addToCart = (product) => {
+    loadModal();
+    if (!cart.some((item) => item.id === product.id)) {
+      setCart([...cart, product]);
+    } else {
+      let cartItem = cart.find((item) => item.id == product.id);
+      cartItem.quantity = cartItem.quantity + 1;
+      setCart(cart);
+    }
+  };
+
+  const onChangeCartFromModal = (product, quantity) => {
+    console.log("You've have changed the Cart");
+  };
+
   return (
     <Layout>
-      <Content />
+      <Content onAddToCart={addToCart} />
+      <Modal
+        cart={cart}
+        onChangeCart={onChangeCartFromModal}
+        showModal={showModal}
+        setShowModal={setShowModal}
+      />
     </Layout>
   );
 };
