4147157d52632f065180c6771bf248f1505ae30d
Chigozie Orunta <chigozieorunta@yahoo.com>
2021-02-21 12:41:02 +0100

    Refactored Counter component

--- src/components/modal/Counter.js
+++ src/components/modal/Counter.js
@@ -1,33 +1,5 @@
 import React, { useState, useEffect } from "react";
 
-const counterStyle = {
-  marginTop: 10,
-  borderRadius: 0,
-  border: "1px solid #000",
-  padding: "3px 0 8px 0",
-  fontSize: 15,
-  textAlign: "center",
-  width: 75,
-  display: "flex",
-  justifyContent: "space-around",
-};
-
-const counterMinusStyle = {
-  display: "block",
-  cursor: "pointer",
-  position: "relative",
-  top: 0,
-};
-
-const counterPlusStyle = {
-  display: "block",
-  cursor: "pointer",
-  position: "relative",
-  top: 1.5,
-};
-
-const countStyle = { position: "relative", top: 2 };
-
 const Counter = ({ quantity, onCounterPlus, onCounterMinus }) => {
   const [count, setCount] = useState(quantity);
 
@@ -36,16 +8,46 @@ const Counter = ({ quantity, onCounterPlus, onCounterMinus }) => {
   }, [quantity]);
 
   return (
-    <div style={counterStyle}>
-      <span style={counterMinusStyle} onClick={onCounterMinus}>
+    <div style={styles.counterStyle}>
+      <span style={styles.counterMinusStyle} onClick={onCounterMinus}>
         -
       </span>
-      <span style={countStyle}>{count}</span>
-      <span style={counterPlusStyle} onClick={onCounterPlus}>
+      <span style={styles.countStyle}>{count}</span>
+      <span style={styles.counterPlusStyle} onClick={onCounterPlus}>
         +
       </span>
     </div>
   );
+};
+
+const styles = {
+  countStyle: { position: "relative", top: 2 },
+
+  counterPlusStyle: {
+    display: "block",
+    cursor: "pointer",
+    position: "relative",
+    top: 1.5,
+  },
+
+  counterMinusStyle: {
+    display: "block",
+    cursor: "pointer",
+    position: "relative",
+    top: 0,
+  },
+
+  counterStyle: {
+    marginTop: 10,
+    borderRadius: 0,
+    border: "1px solid #000",
+    padding: "3px 0 8px 0",
+    fontSize: 15,
+    textAlign: "center",
+    width: 75,
+    display: "flex",
+    justifyContent: "space-around",
+  },
 };
 
 export default Counter;
