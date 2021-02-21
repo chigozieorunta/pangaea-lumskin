1d52ab8bf214cfb029640daa906899c76700570c
Chigozie Orunta <chigozieorunta@yahoo.com>
2021-02-21 14:00:41 +0100

    Refactored styling for TopNavigationBar compnent

--- src/components/header/TopNavigationBar.js
+++ src/components/header/TopNavigationBar.js
@@ -1,26 +1,14 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import Logo from "./Logo";
 import TopLeftNavigation from "./TopLeftNavigation";
 import TopRightNavigation from "./TopRightNavigation";
-
-const headerStyle = {
-  paddingTop: "0.5em",
-  paddingBottom: "0.5em",
-  boxShadow: "0 2px 3px -3px grey",
-};
-
-const colStyle = {
-  display: "flex",
-  alignItems: "center",
-  padding: "0 2em",
-};
+import Logo from "./Logo";
 
 const TopNavigationBar = () => {
   return (
-    <Container style={headerStyle} fluid>
+    <Container style={styles.headerStyle} fluid>
       <Row>
-        <Col style={colStyle}>
+        <Col style={styles.colStyle}>
           <Logo />
           <TopLeftNavigation />
           <TopRightNavigation />
@@ -28,6 +16,20 @@ const TopNavigationBar = () => {
       </Row>
     </Container>
   );
+};
+
+const styles = {
+  headerStyle: {
+    paddingTop: "0.5em",
+    paddingBottom: "0.5em",
+    boxShadow: "0 2px 3px -3px grey",
+  },
+
+  colStyle: {
+    display: "flex",
+    alignItems: "center",
+    padding: "0 2em",
+  },
 };
 
 export default TopNavigationBar;
