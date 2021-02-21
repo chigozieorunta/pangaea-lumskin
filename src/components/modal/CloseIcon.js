ed97178f4645e5f6d2a121f2e462a6839457b10e
Chigozie Orunta <chigozieorunta@yahoo.com>
2021-02-21 12:36:17 +0100

    Refactored CloseIcon component

--- src/components/modal/CloseIcon.js
+++ src/components/modal/CloseIcon.js
@@ -2,23 +2,37 @@ import React from "react";
 
 const CloseIcon = ({ onClick }) => {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 492.004 492.004"
-      style={{
-        position: "absolute",
-        top: 0,
-        left: 0,
-        height: 15,
-        width: 10,
-        fill: "rgb(43, 46, 43)",
-        cursor: "pointer",
-      }}
-      onClick={onClick}
-    >
-      <path d="M382.678,226.804L163.73,7.86C158.666,2.792,151.906,0,144.698,0s-13.968,2.792-19.032,7.86l-16.124,16.12    c-10.492,10.504-10.492,27.576,0,38.064L293.398,245.9l-184.06,184.06c-5.064,5.068-7.86,11.824-7.86,19.028    c0,7.212,2.796,13.968,7.86,19.04l16.124,16.116c5.068,5.068,11.824,7.86,19.032,7.86s13.968-2.792,19.032-7.86L382.678,265    c5.076-5.084,7.864-11.872,7.848-19.088C390.542,238.668,387.754,231.884,382.678,226.804z"></path>
-    </svg>
+    <section style={styles.iconCircleStyle} onClick={onClick}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 492.004 492.004"
+        style={styles.iconStyle}
+      >
+        <path d="M382.678,226.804L163.73,7.86C158.666,2.792,151.906,0,144.698,0s-13.968,2.792-19.032,7.86l-16.124,16.12    c-10.492,10.504-10.492,27.576,0,38.064L293.398,245.9l-184.06,184.06c-5.064,5.068-7.86,11.824-7.86,19.028    c0,7.212,2.796,13.968,7.86,19.04l16.124,16.116c5.068,5.068,11.824,7.86,19.032,7.86s13.968-2.792,19.032-7.86L382.678,265    c5.076-5.084,7.864-11.872,7.848-19.088C390.542,238.668,387.754,231.884,382.678,226.804z"></path>
+      </svg>
+    </section>
   );
+};
+
+const styles = {
+  iconStyle: {
+    height: 15,
+    width: 10,
+    fill: "rgb(43, 46, 43)",
+  },
+  iconCircleStyle: {
+    height: 25,
+    width: 25,
+    border: "1px solid #999",
+    borderRadius: "100%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    top: 0,
+    left: 0,
+    cursor: "pointer",
+  },
 };
 
 export default CloseIcon;
