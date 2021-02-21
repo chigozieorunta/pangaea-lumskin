c06078feeb748b08d1183e946aa1845b838f0557
Chigozie Orunta <chigozieorunta@yahoo.com>
2021-02-21 13:59:18 +0100

    Housekeeping for Home component

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Layout from "../layout/Layout";
 import Content from "../components/content/Content";
 import Modal from "../components/modal/Modal";
