3d3e3e18092e806e4da94b82360b27cab9eebcd8
Chigozie Orunta <chigozieorunta@yahoo.com>
2021-02-23 12:37:12 +0100

    Refactored Products component to contain currency changes

--- src/components/product/Products.js
+++ src/components/product/Products.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { useQuery } from "@apollo/client";
-import { GET_PRODUCTS } from "../../utils/GetProducts";
 import ProductItem from "./ProductItem";
 
-const Products = ({ onAddToCart }) => {
-  const { loading, error, data } = useQuery(GET_PRODUCTS);
+import { gql, useQuery } from "@apollo/client";
+import { GetProducts } from "../../utils/GetProducts";
+
+const Products = ({ onAddToCart, currency }) => {
+  const { loading, error, data } = useQuery(GetProducts(gql, currency));
 
   if (loading) {
     return <p>Loading...</p>;
@@ -14,7 +15,7 @@ const Products = ({ onAddToCart }) => {
     return <p>${error}</p>;
   }
 
-  const updatedProducts = data.products.map((product, idx) => {
+  const updatedProducts = data.products.map((product) => {
     return Object.assign({ quantity: 1 }, product);
   });
 
@@ -25,6 +26,7 @@ const Products = ({ onAddToCart }) => {
         key={idx}
         product={product}
         onAddToCart={onAddToCart}
+        currency={currency}
         {...product}
       />
     ));
