2e53b75dbff77769e5a63fa96a454a3773aed98d
Chigozie Orunta <chigozieorunta@yahoo.com>
2021-02-23 13:25:31 +0100

    Fixed image resizing on Logo component

--- src/components/header/Logo.js
+++ src/components/header/Logo.js
@@ -4,7 +4,7 @@ import ImageLogo from "../../images/logo.png";
 
 const Logo = () => {
   return (
-    <div style={{ width: "12.5%" }}>
+    <div style={{ width: "12.5%", minWidth: 150, marginLeft: 15 }}>
       <Image style={{ width: 150 }} alt="Lumskin Logo" src={ImageLogo} fluid />
     </div>
   );
