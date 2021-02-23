8bfaa9731087a698844b7c1d12e4b336139b712e
Chigozie Orunta <chigozieorunta@yahoo.com>
2021-02-23 12:56:13 +0100

    Renamed Index component on import path

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Index from "../src/pages/index";
+import Index from "../src/pages/Index";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/index.css";
 
